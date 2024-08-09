c7b6ebfb156bf6585c124011a26dfd4c1ec0c9ff
1n11it <xalpha369@gmail.com>
2024-08-09 13:14:18 +0530

    Added dntCARE files

---
+++ dntCARE/blog/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
+from blog.config import Config
+
+load_dotenv()
+
+csrf = CSRFProtect()
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category= 'info font-weight-semi-bold'
+mail =Mail()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    csrf.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from blog.users.routes import users
+    from blog.posts.routes import posts
+    from blog.main.routes import main
+    from blog.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
